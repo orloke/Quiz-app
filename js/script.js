89aad5eca4e546a7bd854e4b8887db16f1645629
Júnior Dering <juniordering@hotmail.com>
2022-03-02 13:40:04 -0400

    enviando arquivos finalizados

---
+++ js/script.js
@@ -0,0 +1,146 @@
+const quizData = [
+    {
+        question: 'What is your name?',
+        a: 'junior',
+        b: 'dering',
+        c: 'demétrio',
+        d: 'lima',
+        correct: 'a'
+    },
+    {
+        question: 'How old are you?',
+        a: '30',
+        b: '27',
+        c: '26',
+        d: '29',
+        correct: 'c'
+    },
+    {
+        question: 'What name your mother?',
+        a: 'Clara',
+        b: 'Fernanda',
+        c: 'Maria',
+        d: 'Regina',
+        correct: 'd'
+    }
+]
+
+const questao = document.getElementById('questao')
+
+const answer_a = document.getElementById('answer_a')
+const answer_b = document.getElementById('answer_b')
+const answer_c = document.getElementById('answer_c')
+const answer_d = document.getElementById('answer_d')
+const button = document.getElementById('send')
+const reload = document.getElementById('reload')
+const add = document.getElementById('add')
+const question_ask = document.getElementById('question-ask')
+const q_a = document.getElementById('question-a') 
+const q_b = document.getElementById('question-b') 
+const q_c = document.getElementById('question-c') 
+const q_d = document.getElementById('question-d')
+const q_correct = document.getElementById('question-correct')
+const add_question = document.getElementById('add-question') 
+
+
+let currentQuestion = 0
+let score = 0
+
+Deselect()
+loadQuiz()
+Start()
+
+function loadQuiz(){
+    reload.style.display = 'none'
+    correntquizData = quizData[currentQuestion]
+    questao.innerText = correntquizData.question
+    answer_a.innerText = correntquizData.a
+    answer_b.innerText = correntquizData.b
+    answer_c.innerText = correntquizData.c
+    answer_d.innerText = correntquizData.d
+}
+
+function Select(){
+    const ans = document.querySelectorAll('.point')
+    ans.forEach(item=>{
+        if(item.checked){
+            if(item.value == correntquizData.correct){
+                score++
+                
+            }
+        }
+    })
+}
+
+function Deselect(){
+    const ans = document.querySelectorAll('.point')
+    ans.forEach(item=>{
+        item.checked = false
+    })
+}
+
+function Start(){
+    button.addEventListener('click',()=>{
+        console.log(currentQuestion)
+        currentQuestion++
+        if(currentQuestion<quizData.length){
+            Select()
+            loadQuiz()
+            Deselect()        
+        }
+    
+        if(currentQuestion == quizData.length-1){
+            button.innerText = 'Concluir'  
+        }
+        
+        if(currentQuestion == quizData.length){
+            Select()
+            alert('Você respondeu todas as quetões sua pontuação foi: ' + score)
+            button.style.display = 'none'
+            reload.style.display = 'block'
+        }
+    })
+}
+
+
+reload.addEventListener('click',()=>{
+    button.style.display = 'block'
+    button.innerText = 'Enviar'
+    reload.style.display = 'none'
+    score = 0
+    currentQuestion = 0
+    loadQuiz()
+    Deselect()
+})
+
+add.addEventListener('click',()=>{
+    const newQuestion = document.getElementsByClassName('new-question')[0]
+    if(newQuestion.getAttribute('style') == "display: none;"){
+        newQuestion.style.display = 'flex'
+    }else{
+        newQuestion.style.display = 'none'
+    }    
+})
+
+add_question.addEventListener('click',()=>{
+    let new_question ={
+        question: question_ask.value,
+        a: q_a.value,
+        b: q_b.value,
+        c: q_c.value,
+        d: q_d.value,
+        correct: q_correct.value
+    }
+
+    quizData.push(new_question)
+    for (a in new_question){
+        console.log(new_question[a])
+    }
+
+    question_ask.value = ''
+    q_a.value =''
+    q_b.value =''
+    q_c.value =''
+    q_d.value =''
+    q_correct.value =''
+})
